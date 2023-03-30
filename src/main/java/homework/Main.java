4f24e7a15fbb4d6694bc2d38c0d35672d9e1595c
Nikita Novikov <monstars51@gmail.com>
2023-03-30 13:10:34 +0300

    add Task2

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -7,20 +7,61 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        //TODO Task1
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
         writeString(json);
+
+        //TODO Task2
+        List<Employee> list2 = parseXML("data.xml");
+        String json2 = listToJson(list2);
+        writeString(json2);
+    }
+
+    public static List<Employee> parseXML(String s) throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> employeeList = new ArrayList<>();
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(s));
+
+        //Получим корневой узел документа:
+        Node root = doc.getDocumentElement();
+        //Получить список дочерних узлов можно при помощи метода getChildNodes():
+        NodeList nodeList = root.getChildNodes();
+        //Пробежаться по всем дочерним узлам текущего узла можно с помощью цикла:
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            if (Node.ELEMENT_NODE == node.getNodeType()) {
+                Element element = (Element) node;
+                long id = Integer.parseInt(element.getElementsByTagName("id").item(0).getTextContent());
+                String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                String country = element.getElementsByTagName("country").item(0).getTextContent();
+                int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+                Employee employee = new Employee(id, firstName, lastName, country, age);
+                employeeList.add(employee);
+            }
+        }
+        return employeeList;
     }
 
     public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
@@ -53,7 +94,7 @@ public class Main {
     }
 
     public static void writeString(String text) {
-        try (FileWriter writer = new FileWriter("lectureResources/data.json")) {
+        try (FileWriter writer = new FileWriter("data.json")) {
             writer.write(text);
             writer.flush();
         } catch (IOException ex) {
